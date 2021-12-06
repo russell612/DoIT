720993b0e1fecad7312e698d7898bfa7027b4a59
Russell Kong <russellkong@hotmail.co.uk>
2021-12-06 22:39:25 +0800

    Changed Event Value

--- code/PyGUI.py
+++ code/PyGUI.py
@@ -5,7 +5,7 @@ window = sg.Window("Demo",layout)
 
 while True:
     event, values = window.read()
-    if event == "OK" or event == sg.WIN_CLOSED:
+    if event == "Lol gay" or event == sg.WIN_CLOSED:
         break
 
 window.close()
