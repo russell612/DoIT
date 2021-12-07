0dc978478d26990c8fad0d76db5c819d062c177a
jkjk807 <limjingkai123@gmail.com>
2021-12-07 15:39:52 +0800

    Added 'Clear','Refresh', and load function

--- code/main.py
+++ code/main.py
@@ -26,12 +26,11 @@ layout = \
     [sg.Button("Complete Task",visible = False, key= '_complete_')],
     [sg.Button("Refresh")],
     [sg.Button("Clear")],
-
     [sg.Button("Exit")]]
 window = sg.Window("To-Do-List-Randomizer", layout)
 
 def add_to_list(task_list, index):
-    input = "{}. ".format(index) + values["input"] + "\n"
+    input = "{} ".format(index) + values["input"] + "\n"
     task_list += input
     index += 1
     window["output"].update(task_list)
@@ -39,14 +38,17 @@ def add_to_list(task_list, index):
 print(task_list)
 
 while True:
+
     event, values = window.read()  
+    window["_output_"].update(task_list)
+
 
     if event =="Clear":
         filehandler = open('tasklist.txt','wb') 
         task_list=empty_list
         pickle.dump(task_list,filehandler) 
         filehandler.close() #closes the file
-
+        window["_output_"].update(task_list)
 
     if event =="Refresh":
         window["_output_"].update(task_list)      
@@ -55,14 +57,13 @@ while True:
         window['_complete_'].update(visible = True)
         pass #ToDo: randomize function 
             #randomize tasks based on the file given
+
     if event == "Add To List":
-
-        input = "{}. ".format(values["_input_"]) + "\n" # Gives an index with the values from the input text and adds \n to create a new line
-
-
+        input = "{} ".format(values["_input_"]) + "\n" # Gives an index with the values from the input text and adds \n to create a new line
         task_list += input # Concatenates it into the task_list string
         index += 1
         window["_output_"].update(task_list)     
+
     if event == "Exit" or event == sg.WIN_CLOSED:
         filehandler = open('tasklist.txt','wb') #opens tasklist file to save the tasks inputted
         pickle.dump(task_list,filehandler) #saves the the tasks inputted into the tasklist file
