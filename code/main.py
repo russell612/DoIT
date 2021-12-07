60ce98523216e1b423dddf696bddee57db6bff5e
Russell Kong <russellkong@hotmail.co.uk>
2021-12-07 14:48:38 +0800

    v0.2.9

--- code/main.py
+++ code/main.py
@@ -1,5 +1,5 @@
-### Current Version: v0.2.8
-### Patch Notes: Added Comments
+### Current Version: v0.2.9
+### Patch Notes: Changed Layout into multiple lines for easy visibility
 
 import PySimpleGUI as sg
 import pickle
@@ -14,7 +14,14 @@ index = 1
 #ToDo: Load Function
 ### Opens the Stored File and Loads it into the Interface.
 
-layout = [[sg.Text("Current Tasks:")],[sg.Text(key = "output")],[sg.Text("Type your Input Here:")], [sg.Input(key = "input", do_not_clear=False)], [sg.Button("Add To List"), sg.Button("Randomize Task")], [sg.Button("Complete Task",visible = False, key= '_complete_')],[sg.Button("Exit")]]
+layout = \
+    [[sg.Text("Current Tasks:")],
+    [sg.Text(key = "_output_")],
+    [sg.Text("Type your Input Here:")], 
+    [sg.Input(key = "_input_", do_not_clear=False)], 
+    [sg.Button("Add To List"), sg.Button("Randomize Task")], 
+    [sg.Button("Complete Task",visible = False, key= '_complete_')],
+    [sg.Button("Exit")]]
 window = sg.Window("To-Do-List-Randomizer", layout)
 
 def add_to_list(task_list, index):
@@ -30,10 +37,10 @@ while True:
         pass #ToDo: randomize function 
             #randomize tasks based on the file given
     if event == "Add To List":
-        input = "{}. ".format(index) + values["input"] + "\n" # Gives an index with the values from the input text and adds \n to create a new line
+        input = "{}. ".format(index) + values["_input_"] + "\n" # Gives an index with the values from the input text and adds \n to create a new line
         task_list += input # Concatenates it into the task_list string
         index += 1
-        window["output"].update(task_list)     
+        window["_output_"].update(task_list)     
     if event == "Exit" or event == sg.WIN_CLOSED:
         filehandler = open('tasklist.txt','wb') #opens tasklist file to save the tasks inputted
         pickle.dump(task_list,filehandler) #saves the the tasks inputted into the tasklist file
