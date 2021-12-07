049082f5dd607dff445ae3006ccc8b5d7c4e8510
Russell Kong <russellkong@hotmail.co.uk>
2021-12-07 10:06:38 +0800

    v0.2.0

--- code/main.py
+++ code/main.py
@@ -5,12 +5,14 @@
 import PySimpleGUI as sg
 import pickle
 
+#Global Variables
+task_list = []
+sg.theme('SystemDefaultForReal')
+
 #ToDo: Load Function
 ### Opens the Stored File and Loads it into the Interface.
 
-input = [sg.Input()]
-
-layout = [[sg.Text("Current Tasks:")],[sg.Text("Type your Input Here:")], input, [sg.Button("Add To List")], [sg.Button("Randomize Task")], [sg.Button("Exit")]]
+layout = [[sg.Text("Current Tasks:")],[sg.Text("Type your Input Here:")], [sg.Input(key = "input")], [sg.Button("Add To List"), sg.Button("Randomize Task"), sg.Button("Exit")]]
 window = sg.Window("To-Do-List-Randomizer", layout)
 
 while True:
@@ -19,7 +21,8 @@ while True:
         pass #ToDo: randomize function 
             #randomize tasks based on the file given
     if event == "Add To List":
-        pass #ToDo: addToList function
+        input = values("input")
+        task_list.append(input)       
     if event == "Exit" or event == sg.WIN_CLOSED:
         break #ToDo: Save function 
 
