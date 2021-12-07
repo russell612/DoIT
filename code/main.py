799c35ae7c4e78351392c1d48f41cb5b6cd8d220
RyanKoh03 <ryankoh66@gmail.com>
2021-12-07 11:50:28 +0800

    added complete task button

--- code/main.py
+++ code/main.py
@@ -5,6 +5,8 @@
 import PySimpleGUI as sg
 import pickle
 
+from PySimpleGUI.PySimpleGUI import Window
+
 #Global Variables
 task_list = ''
 sg.theme('SystemDefaultForReal')
@@ -13,13 +15,13 @@ index = 1
 #ToDo: Load Function
 ### Opens the Stored File and Loads it into the Interface.
 
-layout = [sg.Text("Current Tasks:")],[sg.Text(key = "output")],[sg.Text("Type your Input Here:")], [sg.Input(key = "input", do_not_clear=False)], [sg.Button("Add To List"), sg.Button("Randomize Task")], sg.Button("Complete Task",visible = False) ,sg.Button("Exit")
+layout = [[sg.Text("Current Tasks:")],[sg.Text(key = "output")],[sg.Text("Type your Input Here:")], [sg.Input(key = "input", do_not_clear=False)], [sg.Button("Add To List"), sg.Button("Randomize Task")], [sg.Button("Complete Task",visible = False, key= '_complete_')],[sg.Button("Exit")]]
 window = sg.Window("To-Do-List-Randomizer", layout)
 
 while True:
     event, values = window.read()
     if event == "Randomize Task":
-        update("Complete Task", visible = True)
+        window['_complete_'].update(visible = True)
         pass #ToDo: randomize function 
             #randomize tasks based on the file given
     if event == "Add To List":
