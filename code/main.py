00033a2a9aa87203dc6fdddb08965b8bc5877f8b
L1MJH <37625953+LimJH2002@users.noreply.github.com>
2021-12-08 21:57:05 +0800

    Complete function done with a few improved feedbacks

--- code/main.py
+++ code/main.py
@@ -1,5 +1,7 @@
-### Current Version: v0.3.2
-### Patch Notes: Smashed Bug for Randomize, Complete Task is now always visible however
+### Current Version: v0.4.2
+### Patch Notes: Complete function done. Improved logic when task list is empty and user wants to randomize
+###              Also improved feedback when users finish all tasks
+
 import PySimpleGUI as sg
 import pickle
 import random
@@ -49,14 +51,30 @@ def randomize(string_list):
     return list_to_randomize
 
 def task_random(string_list):
+    global specific_task
     randomized_list = randomize(string_list)
-    specific_task = randomized_list.pop(0)
-    task_list = empty_list
+    specific_task = randomized_list[0]
+    # task_list = empty_list
     # for task in randomized_list:
     #     task_list += "{} ".format(task) + '\n'
     window["_random_"].update(specific_task)
     # window["_output_"].update(task_list)
+    # return task_list
 
+
+def clear_task_after_done(string_list):
+    global task_list
+    temp_list = string_list.splitlines()
+    # print(temp_list)
+    # specific_task = randomized_list[0]
+    # print(specific_task)
+    for task in temp_list:
+        if task == specific_task:
+            temp_list.remove(task)
+    task_list = empty_list
+    for leftover in temp_list:
+        task_list += "{} ".format(leftover) + '\n'
+    window["_output_"].update(task_list)
     return task_list
     
 
@@ -76,22 +94,32 @@ while True:
         window["_output_"].update(task_list)    
         
 
-    if event == "Randomize Task":
+    if event == "Randomize Task" and len(task_list) != 0:
         if is_done == True:
             task = task_random(task_list)
             is_done = False
         else:
             window["_output_"].update("Please finish the current task first!")
 
+    if event == "Randomize Task" and len(task_list) == 0:
+        window["_output_"].update("Please input a task.")
+
+
+
     if event == "Complete Task" and is_done == False:
         is_done = True
+        clear_task_after_done(task_list)
 
+
+    if event == "Complete Task" and len(task_list) == 0:
+        window["_output_"].update("Congratulations, you have completed all your tasks!")
         
 
+        
     if event == "Add To List":
         input = "{} ".format(values["_input_"]) + "\n" # Gives an index with the values from the input text and adds \n to create a new line
         task_list += input # Concatenates it into the task_list string
-        window["_output_"].update(task_list)     
+        window["_output_"].update(task_list)    
 
 
     if event == "Exit" or event == sg.WIN_CLOSED:
